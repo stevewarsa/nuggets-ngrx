5519c310f36e5a21bba9e85251fa85b986a60fb3
Steve Warsa <swarsa480@gmail.com>
2020-04-22 05:54:43 -0700

    Modified the get_quote_id_list PHP file to read from new table and removed the get_quote_list script and added a new get_quote_by_id PHP file

--- server/get_quote_by_id.php
+++ server/get_quote_by_id.php
@@ -3,14 +3,16 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf8');
 
 $user = $_GET['user'];
+$quoteId = $_GET['quote_id'];
 
 $db = new SQLite3('db/memory_' . $user . '.db');
 
-$results = $db->query("SELECT quote_id, create_dt FROM quote");
+$results = $db->query("SELECT quote_id, quote_tx, create_dt FROM quote where quote_id = " . $quoteId);
 $arrayName = array();
 while ($row = $results->fetchArray()) {
     $quote = new stdClass;
     $quote->quoteId = $row['quote_id'];
+    $quote->quoteTx = $row['quote_tx'];
     $quote->createDt = $row['create_dt'];
     array_push($arrayName, $quote);
 }
